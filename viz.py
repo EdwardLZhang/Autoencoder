60febe295ccfbceb677e843b35bcf772509c15fe
Edward Zhang <elzhang2006@gmail.com>
2025-12-28 18:52:48 -0500

    finished visualization

--- viz.py
+++ viz.py
@@ -4,21 +4,37 @@ from model import AutoEncoder
 import matplotlib.pyplot as plt
 from loaddata import loadCIFAR10
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def visualize_autoencoding(autoencoder, testdata, num_images=5):
+    images, _ = next(iter(testdata))
+    images = images.to(device)
 
-# load the model
-autoencoder: AutoEncoder = AutoEncoder()
-state = torch.load('autoencoder.pth', map_location='cpu')
-autoencoder.load_state_dict(state)
-autoencoder = autoencoder.to(device)
-autoencoder.eval()
+    with torch.no_grad():
+        reconstructed = autoencoder(images)
 
-# load the test data
+    fig, axes = plt.subplots(num_images, 2)
 
-traindata, testdata = loadCIFAR10()
+    for i in range(min(num_images, len(images))):
+        axes[i, 0].imshow(images[i].cpu().permute(1, 2, 0))
+        axes[i, 0].axis('off')
 
-images, _ = next(iter(testdata))
-images = images.to(device)
+        axes[i, 1].imshow(reconstructed[i].cpu().permute(1, 2, 0))
+        axes[i, 1].axis('off')
 
-fig, axes = plt.subplots(5, 2)
+    plt.show()
 
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # load the model
+    autoencoder: AutoEncoder = AutoEncoder()
+    state = torch.load('autoencoder.pth', map_location='cpu')
+    autoencoder.load_state_dict(state)
+    autoencoder = autoencoder.to(device)
+    autoencoder.eval()
+
+    # load the test data
+
+    traindata, testdata = loadCIFAR10()
+
+    visualize_autoencoding(autoencoder, testdata)
